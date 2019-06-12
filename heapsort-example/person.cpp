b991f333db17705dd1760cda9b10532060366cf3
nelavara <42851206+nelavara@users.noreply.github.com>
2019-06-11 23:18:44 -0400

    addition of person.cpp

---
+++ heapsort-example/person.cpp
@@ -0,0 +1,30 @@
+//Evan Perry
+//Data Structures
+
+#include "person.hpp"
+
+string person::getName(void)   //Gets name
+{
+	return name;
+}
+
+string person::getPhone(void)  //Gets phone
+{
+	return phone_number;
+}
+
+void person::setName(string nname) //Sets the name
+{
+	name = nname;
+}
+
+void person::setPhone(string pphone)  //Sets the phone
+{
+	phone_number = pphone;
+}
+
+void person::PrintPerson(void)  //Prints the contents of the Person class.
+{
+	cout << "Name: " << name << endl;
+	cout << "Phone: " << phone_number << endl;
+}
